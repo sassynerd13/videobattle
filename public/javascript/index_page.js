dc024f092f636290e0f72c3265f86b9eb299090f
Michael Angelo <yamikamisama@gmail.com>
2015-05-09 18:25:10 -0400

    add basic data pull

--- public/javascript/index_page.js
+++ public/javascript/index_page.js
@@ -3,6 +3,17 @@ $(document).ready(function() {
         interval: 5000 //changes the speed
     })
 
+database.on('value', function(snap){
+  var data = snap.val()
+  // data.battles
+  debugger
 
+  var currentBattles = $('.carousel-inner').children();
+  i = 0
+  currentBattles.each(function(){
+    // this.attr(src, )
+    i++;
+  });
+})
 
 });
