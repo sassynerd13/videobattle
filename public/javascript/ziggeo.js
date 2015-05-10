8ae1ea722c71b56754a7b62627a25a6e023c65b8
Michael Angelo <yamikamisama@gmail.com>
2015-05-09 20:25:47 -0400

    add ziggeo firebase logic

--- public/javascript/ziggeo.js
+++ public/javascript/ziggeo.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
 
   ZiggeoApi.Events.on("submitted", function(data) {
     $(that).replaceWith('<ziggeo ziggeo-video="' + data.video.token + '" ziggeo-width=320 ziggeo-height=240></ziggeo>');
+    addVideo('ziggeo', data.video.token, data.image);
     $('.modal').removeClass('in').addClass('out');
     $('.begin').show();
     $('.zig2').remove();
