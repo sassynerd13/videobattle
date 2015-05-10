a01d73880530da2a47726e409f061bc28870283b
Michael Angelo <yamikamisama@gmail.com>
2015-05-09 20:02:03 -0400

    ziggeo and event listener

--- public/javascript/ziggeo.js
+++ public/javascript/ziggeo.js
@@ -1,22 +1,20 @@
 var ziggeo;
 $(document).ready(function() {
-  $('.add-video').on('click', function(){
-  that = this;
-  console.log(that)
+  $('.add-video').on('click', function() {
+    that = this;
+    console.log(that)
   });
-
 
   $('.ziggeo-upload').on("click", function() {
-      $('.video-buttons').hide()
-      $('.zig').append(
-        "<div class='zig2'><ziggeo ziggeo-width=320 ziggeo-height=240></ziggeo></div>")
+    $('.begin').hide()
+    $('.zig').append(
+      "<div class='zig2'><ziggeo ziggeo-width=320 ziggeo-height=240></ziggeo></div>")
   });
 
-  ZiggeoApi.Events.on("submitted", function (data) {
-      $(that).replaceWith('<ziggeo ziggeo-video="' + data.video.token + '" ziggeo-width=320 ziggeo-height=240></ziggeo>');
-      $('.modal').removeClass('in').addClass('out');
-      $('.video-buttons').show()
-      $('.zig2').remove();
-    });
+  ZiggeoApi.Events.on("submitted", function(data) {
+    $(that).replaceWith('<ziggeo ziggeo-video="' + data.video.token + '" ziggeo-width=320 ziggeo-height=240></ziggeo>');
+    $('.modal').removeClass('in').addClass('out');
+    $('.begin').show()
+    $('.zig2').remove();
+  });
 });
-
